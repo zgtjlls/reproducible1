634a9740d926a17a32e73ea571597bbb18717853
linshanli <zgtjlls@gmail.com>
2017-06-18 19:14:28 -0400

    delete

--- project1.R
+++ project1.R
@@ -1,3 +1,6 @@
+## libraries
+library(dplyr)
+
 ## Set work directory and download data
 setwd("~/Documents/Coursera/Data Science/Reproducible Research/project1/reproducible1")
 data_link <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
@@ -9,3 +12,10 @@ if(! file.exists("data.zip")){
 unzip("data.zip")
 data <- read.csv("activity.csv")
 View(data)
+
+## histogram of steps for each day
+table(data$date)
+data_steps <- data %>% group_by(date) %>% summarize(total_step = sum(steps, na.rm = TRUE))
+hist(data_steps$total_step, breaks = 10, 
+     main = "Histogram of total number of steps each day", 
+     xlab = "total number of steps each day")
